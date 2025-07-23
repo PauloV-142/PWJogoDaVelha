455cbf338a3605f2034eafa100469deed4efbe25
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2025-07-23 14:40:35 -0300

    TudoPronto .v1

--- jogo.js
+++ jogo.js
@@ -1,12 +1,11 @@
-let jogador = 0; //Talvez o jogador possa ser 1 e -1, para não precisar do array intVal
-let tab = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+let jogador = 0;
+let tab = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 const imgVal = ['imgo', 'imgx']; //juntar imgVal com imgPontos.
-//const intVal = [-1, 1];
-const winGuide = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]; //Hmm, wait, New Way of doing it!
+const winGuide = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 const txtPontos = {
-    "X": document.getElementById("pontosX"),
-    "O": document.getElementById('pontosO')
+    "X": document.getElementById("txt-pontosX"),
+    "O": document.getElementById('txt-pontosO')
 };
 const imgPontos = [
     document.getElementById('img-PontosX'),
@@ -19,68 +18,64 @@ const quadrados = document.querySelectorAll('.quadrado');
 const btnReiniciar = document.getElementById('reiniciar');
 const txtVitoria = document.getElementById('mensagem');
 
-
 quadrados.forEach(element => {
    element.addEventListener('click',
-      () => jogar(element));//, {once: true}
+      () => jogar(element));
    });
    
-const alternarJogador = num => (++num) % 2;
+const alternarJogador = num => (++num) % 2; // Basicamente retorna 0 ou 1
 
 const msg = texto => txtVitoria.textContent = texto;
 
-function atualizar(slot) {
-   const quadradoId = slot.id; // É melhor eu modificar os ids, e acessar a string para obter o número.
-   tab[quadradoId] = jogador;
-   // txtDebug.textContent = tab;
-}
-
 function buscarDoTabuleiro(linhaGuia) {
-  let linhaDoTab = new Set();//Talvez um array que não permite repetições seria útil.
-  for (let quadrado of linhaGuia) {
-      linhaDoTab.add(tab[quadrado]);
+  let linhaDoTab = new Set();
+  for (let valorQuadrado of linhaGuia) {
+      linhaDoTab.add(tab[valorQuadrado]);
   }
-  return linhaDoTab
+  return linhaDoTab;
 }
 
-function tratarVitoria(strJogador) { //WIP
-   msg(`${strJogador} Ganhou!`)
-   txtPontos[strJogador].textContent = ++txtPontos[strJogador].textContent
-   tab = [];//Para não poder clicar mais. E nem dar trigger na opção de empate.
+function tratarVitoria(strJogador, linhaVencedora) {
+   msg(`${strJogador} Ganhou!`);
+   txtPontos[strJogador].textContent = ++txtPontos[strJogador].textContent; // Adiciona 1 ao contador.
+   
+   function rgbR(){ return Math.random() * 255 };
+
+   linhaVencedora.forEach(coord => { // quadradosVencedores seria melhor OwO
+         quadrados[coord].style.backgroundColor = `rgb(${rgbR()}, ${rgbR()}, ${rgbR()})`
+      })
+   tab = [];//Para não poder clicar mais. E nem dar trigger na condição de empate.
 }
 
 function jogar(slot) {
-if (tab[slot.id] === -1){
-   // console.log(slot.id)
+const quadradoId = slot.id
+if (tab[quadradoId] === -1){ // Verifica se o quadrado não está ocupado.
    jogador = alternarJogador(jogador);
-   jog = imgVal[jogador];
-   slot.classList.add(jog);
+   tab[quadradoId] = jogador
+
+   slot.classList.add(imgVal[jogador]); // Coloca a imagem no quadrado (slot).
    imgPontos[jogador].classList.remove('inativo');
    imgPontos[alternarJogador(jogador)].classList.add('inativo');
-   atualizar(slot);
    
    let vencedor = false;
+   let linhaVencedora = [];
    for (let linhaGuia of winGuide) {
-      let linha = buscarDoTabuleiro(linhaGuia)
+      let linha = buscarDoTabuleiro(linhaGuia);
       
-      if (linha.size == 1) {
+      if (linha.size == 1 && !linha.has(-1)) {
+         linhaVencedora = linhaVencedora.concat(linhaGuia)
          if (linha.has(0)) {
-            // pontosX.textContent = ++pontosX.textContent;
-            // console.log(`PontosO ${pontosX.textContent}`)
-            // tratarVitoria('X');
             vencedor = 'O';
-         } else if (linha.has(1)) {
-            //pontosO.textContent = ++pontosO.textContent;
-            // console.log(`PontosO ${pontosO.textContent}`)
-            // tratarVitoria('O');
+         } else {
             vencedor = 'X';
          } 
       }
    }
    if (vencedor) {
-      tratarVitoria(vencedor)
-   } else if ((!(tab.includes(-1))) && tab.includes(1)) {
-      msg(`Empate ${tab}`)
+      tratarVitoria(vencedor, linhaVencedora)
+      console.log(linhaVencedora)
+   } else if (!tab.includes(-1) && tab.includes(1)) {
+      msg("Empate!")
    }
 }}
 
@@ -88,8 +83,12 @@ function reiniciar() {
    quadrados.forEach(element => {
       element.classList.remove(imgVal[0], imgVal[1])
    });
-   tab = [0,0,0,0,0,0,0,0,0];
+   tab = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
    txtVitoria.textContent = 'msg';
+   quadrados.forEach(elemento => {
+      elemento.style.backgroundColor = ''
+   }
+   )
    // txtDebug.textContent = tab;
 }
 
